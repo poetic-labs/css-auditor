362b02d074f45d5bf4ae2fc940f098336f68f510
Evan Corl <evancorl@yahoo.com>
2017-06-28 01:27:11 -0500

    Add intrinsic-width feature values

--- src/maps/features/values.js
+++ src/maps/features/values.js
@@ -1,5 +1,15 @@
 /* eslint-disable quote-props */
 
 export default {
-
+  '-moz-fit-content': 'intrinsic-width',
+  '-moz-max-content': 'intrinsic-width',
+  '-moz-min-content': 'intrinsic-width',
+  '-webkit-fill-available': 'intrinsic-width',
+  '-webkit-fit-content': 'intrinsic-width',
+  '-webkit-max-content': 'intrinsic-width',
+  '-webkit-min-content': 'intrinsic-width',
+  'max-content': 'intrinsic-width',
+  'min-content': 'intrinsic-width',
+  'fill-available': 'intrinsic-width',
+  'fit-content': 'intrinsic-width',
 };
