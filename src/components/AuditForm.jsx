953ff9bc0eb29c288929f6439ab1887597ed08dc
Evan Corl <evancorl@yahoo.com>
2017-07-10 12:59:34 -0500

    Remove target browsers from AuditForm

--- src/components/AuditForm.jsx
+++ src/components/AuditForm.jsx
@@ -3,21 +3,23 @@ import React from 'react';
 
 const AuditForm = ({ css, onChangeCss, onSubmit }) => (
   <form className="aud-form" onSubmit={onSubmit}>
-    <h1 className="aud-h1">CSS Auditor</h1>
-    <textarea
-      autoFocus // eslint-disable-line jsx-a11y/no-autofocus
-      className="aud-textarea inner"
-      onChange={onChangeCss}
-      placeholder="Paste CSS here to audit..."
-      value={css}
-    />
-    <div>
-      <button
-        type="submit"
-        className="aud-btn-primary submit-btn"
-      >
-        Audit
-      </button>
+    <div className="inner">
+      <h1 className="aud-h1">CSS Auditor</h1>
+      <textarea
+        autoFocus // eslint-disable-line jsx-a11y/no-autofocus
+        className="aud-textarea"
+        onChange={onChangeCss}
+        placeholder="Paste CSS here to audit..."
+        value={css}
+      />
+      <div>
+        <button
+          type="submit"
+          className="aud-btn-primary submit-btn"
+        >
+          Audit
+        </button>
+      </div>
     </div>
   </form>
 );
