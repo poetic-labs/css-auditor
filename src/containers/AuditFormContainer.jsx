54331e142210b8f94b9dd14a7a779842759382cf
Evan Corl <evancorl@yahoo.com>
2017-07-10 10:32:28 -0500

    Create handler for browser selection changes

--- src/containers/AuditFormContainer.jsx
+++ src/containers/AuditFormContainer.jsx
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AuditForm from '../components/AuditForm';
 import audit from '../audit';
+import browserVersions from '../browsers/browserVersions';
 
 class AuditFormContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      browserSelections: {},
       css: '',
     };
 
+    this.onChangeBrowsers = this.onChangeBrowsers.bind(this);
     this.onChangeCss = this.onChangeCss.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeBrowsers(changedBrowserSelections) {
+    this.setState({
+      browserSelections: {
+        ...this.state.browserSelections,
+        ...changedBrowserSelections,
+      },
+    });
   }
 
   onChangeCss(event) {
@@ -23,9 +35,10 @@ class AuditFormContainer extends Component {
     event.preventDefault();
 
     const { onAudit } = this.props;
+    const { css, browserSelections } = this.state;
 
     try {
-      const auditSummary = audit(this.state.css);
+      const auditSummary = audit(css, browserSelections);
 
       onAudit(auditSummary);
     } catch (error) {
@@ -34,11 +47,14 @@ class AuditFormContainer extends Component {
   }
 
   render() {
-    const { css } = this.state;
+    const { browserSelections, css } = this.state;
 
     return (
       <AuditForm
+        browserSelections={browserSelections}
+        browserVersions={browserVersions}
         css={css}
+        onChangeBrowsers={this.onChangeBrowsers}
         onChangeCss={this.onChangeCss}
         onSubmit={this.onSubmit}
       />
@@ -47,11 +63,7 @@ class AuditFormContainer extends Component {
 }
 
 AuditFormContainer.propTypes = {
-  onAudit: PropTypes.func,
-};
-
-AuditFormContainer.defaultProps = {
-  onAudit: () => {},
+  onAudit: PropTypes.func.isRequired,
 };
 
 export default AuditFormContainer;
