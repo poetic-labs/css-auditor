906d9f4ac3afb5c79c5bbc5dc0299f378ab3256e
Evan Corl <evancorl@yahoo.com>
2017-07-13 00:50:12 -0500

    Use Redux for ModalContainer

--- src/containers/ModalContainer.jsx
+++ src/containers/ModalContainer.jsx
@@ -1,42 +1,15 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import hideModal from '../actions/hideModal';
 import Modal from '../components/Modal';
 
-class ModalContainer extends Component {
-  constructor(props) {
-    super(props);
+const mapStateToProps = ({ isModalVisible }) => ({
+  show: isModalVisible,
+});
 
-    this.state = {
-      show: true,
-    };
+const mapDispatchToProps = dispatch => ({
+  onClickOverlay: () => dispatch(hideModal()),
+});
 
-    this.onClickOverlay = this.onClickOverlay.bind(this);
-  }
-
-  onClickOverlay() {
-    this.setState({ show: false });
-  }
-
-  render() {
-    const { content } = this.props;
-    const { show } = this.state;
-
-    return (
-      <Modal
-        content={content}
-        onClickOverlay={this.onClickOverlay}
-        show={show}
-      />
-    );
-  }
-}
-
-ModalContainer.propTypes = {
-  content: PropTypes.node,
-};
-
-ModalContainer.defaultProps = {
-  content: null,
-};
+const ModalContainer = connect(mapStateToProps, mapDispatchToProps)(Modal);
 
 export default ModalContainer;
