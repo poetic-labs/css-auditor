0eee592097432fc354b303da96bb864e5c9acfe2
Evan Corl <evancorl@yahoo.com>
2017-07-14 11:47:22 -0500

    Combine new reducers into root reducer

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
+import auditSummary from './auditSummary';
+import browserScope from './browserScope';
+import css from './css';
 import isModalVisible from './isModalVisible';
 
 const rootReducer = combineReducers({
+  auditSummary,
+  browserScope,
+  css,
   isModalVisible,
 });
 
