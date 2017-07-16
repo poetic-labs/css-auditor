131b39ab4199e9c7d6232ea8f66ed135f9391a4c
Evan Corl <evancorl@yahoo.com>
2017-07-16 18:58:06 -0500

    Add missing BrowserVersions component

---
+++ src/components/BrowserVersions.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BrowserVersions = () => (
+  <div className="version-selector__container">
+    <h1>Select browser versions</h1>
+  </div>
+);
+
+export default BrowserVersions;
