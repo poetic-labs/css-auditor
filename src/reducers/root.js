88d16eaf8c5cb0986db9879894852992f65613cd
Evan Corl <evancorl@yahoo.com>
2017-07-17 14:37:43 -0500

    Add featureDeclarationLimits to root reducer

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import auditSummary from './auditSummary';
 import browserScope from './browserScope';
 import css from './css';
+import featureDeclarationLimits from './featureDeclarationLimits';
 import modal from './modal';
 
 const rootReducer = combineReducers({
   auditSummary,
   browserScope,
   css,
+  featureDeclarationLimits,
   modal,
 });
 
