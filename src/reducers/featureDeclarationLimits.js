7798ed012e3d0c5a1406935695b6e57e9d258377
Evan Corl <evancorl@yahoo.com>
2017-07-17 14:37:27 -0500

    Create featureDeclarationLimits reducer

---
+++ src/reducers/featureDeclarationLimits.js
@@ -0,0 +1,27 @@
+import {
+  SET_AUDIT_SUMMARY,
+  TOGGLE_FEATURE_DECLARATIONS_LIMIT,
+} from '../actions/constants';
+
+const featureDeclarationLimits = (state = {}, action) => {
+  switch (action.type) {
+    case SET_AUDIT_SUMMARY: {
+      const { featureDeclarations } = action.auditSummary;
+
+      return Object.keys(featureDeclarations)
+        .reduce((limits, feature) => ({
+          ...limits,
+          [feature]: true,
+        }), {});
+    }
+    case TOGGLE_FEATURE_DECLARATIONS_LIMIT:
+      return {
+        ...state,
+        [action.feature]: !state[action.feature],
+      };
+    default:
+      return state;
+  }
+};
+
+export default featureDeclarationLimits;
